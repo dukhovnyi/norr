c05c64bb7a8a024e35261ff1c5305fd75c4cb8f0
Yurii Dukhovnyi <y.dukhovnyi@gmail.com>
2023-01-21 15:43:43 +0200

    Create Xcode project.

---
+++ Pasteboard/PasteboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasteboardApp.swift
+//  Pasteboard
+//
+//  Created by Yurii Dukhovnyi on 21.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct PasteboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
