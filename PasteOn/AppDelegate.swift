4c654a340d4e0fad87af58ab878370df8b8451fa
Yurii Dukhovnyi <ydukhovnyi@Yuriis-Air.lan>
2023-03-25 22:52:07 +0200

    Fix open application form agent.

--- PasteOn/AppDelegate.swift
+++ PasteOn/AppDelegate.swift
@@ -14,7 +14,25 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     let worker: Worker
     var cancellable: AnyCancellable?
-    var panel: FloatingPanel?
+    lazy var panel: FloatingPanel = {
+        let onDidPaste: () -> Void = { [weak self] in
+            self?.panel.orderOut(nil)
+        }
+
+        let dashbaord = Dashboard(
+            viewModel: .init(
+                worker: self.worker,
+                onDidPaste: onDidPaste,
+                analytics: self.analytics
+            )
+        )
+        let contentView = dashbaord.edgesIgnoringSafeArea(.top)
+
+        let _panel = FloatingPanel(contentRect: NSRect(x: 0, y: 0, width: 700, height: 600), backing: .buffered, defer: false)
+        _panel.title = "Floating Panel Title"
+        _panel.contentView = NSHostingView(rootView: contentView)
+        return _panel
+    }()
 
     override init() {
         let preferencesManaging = PreferencesManaging.live()
@@ -29,15 +47,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         defer { worker.start() }
 
         KeyboardShortcuts.onKeyUp(for: .pasteboard) { [weak self] in
-            self?.createFloatingPanel()
-
-            // Center doesn't place it in the absolute center, see the documentation for more details
-            self?.panel?.center()
-
-            // Shows the panel and makes it active
-            self?.panel?.orderFront(nil)
-            self?.panel?.makeKey()
-            self?.panel?.level = .floating
+            self?.presentDashboard()
         }
     }
     
@@ -89,12 +99,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @objc
     private func openApp() {
-        // Center doesn't place it in the absolute center, see the documentation for more details
-        panel?.center()
-
-        // Shows the panel and makes it active
-        panel?.orderFront(nil)
-        panel?.makeKey()
+        presentDashboard()
     }
 
     lazy var staticMenuItems: [NSMenuItem] = [
@@ -104,21 +109,13 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         NSMenuItem(title: "Quit", action: #selector(quit), keyEquivalent: "Q")
     ]
 
-    private func createFloatingPanel() {
-        let onDidPaste: () -> Void = { [weak self] in
-            self?.panel?.orderOut(nil)
-        }
-        // Create the SwiftUI view that provides the window contents.
-        // I've opted to ignore top safe area as well, since we're hiding the traffic icons
-        let dashbaord = Dashboard(
-            viewModel: .init(worker: self.worker, onDidPaste: onDidPaste, analytics: self.analytics)
-        )
-        let contentView = dashbaord
-            .edgesIgnoringSafeArea(.top)
-        // Create the window and set the content view.
-        panel = FloatingPanel(contentRect: NSRect(x: 0, y: 0, width: 700, height: 600), backing: .buffered, defer: false)
-        panel?.title = "Floating Panel Title"
-        panel?.contentView = NSHostingView(rootView: contentView)
+    private func presentDashboard() {
+        // Center doesn't place it in the absolute center, see the documentation for more details
+        panel.center()
+        // Shows the panel and makes it active
+        panel.orderFront(nil)
+        panel.makeKey()
+        panel.level = .floating
     }
 }
 
